b134e68f7edf24163b52274c92167070707e0409
yanivfl <yanivfl@post.bgu.ac.il>
2020-01-07 19:00:12 +0200

    fixed ec2

---
+++ Assignment2/src/main/java/handlers/Constants.java
@@ -0,0 +1,18 @@
+package handlers;
+
+import com.amazonaws.services.sqs.model.Message;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Constants {
+
+    public static final String AMI = "ami-b66ed3de";
+//    public static final String KEY_PAIR = "YuvalKeyPair";
+
+}
+
+
